15e838dc6b4a4bc6bfc24561fc61d5e414379c4e
Dmitri Dmitriev <d.dzmitryeu@gmail.com>
2023-12-12 10:19:15 +0200

    Update function to handle edge cases
    This commit updates the function to handle edge cases, as shown in the git diff.

---
+++ src/ui_components/sectionCards/cardActions.ts
@@ -0,0 +1,35 @@
+import { useContext, useState } from "preact/hooks";
+import BuilderContext from "../../BuilderContext";
+
+const selectedCard = useContext(BuilderContext)?.selectedCard;
+const setSelectedCard = useContext(BuilderContext)?.setSelectedCard;
+const setSelectedSections = useContext(BuilderContext)?.setSelectedSections;
+
+const handleOpenSection = (e: MouseEvent, id: string) => {
+  e.stopPropagation();
+  if (selectedCard === id) {
+    setSelectedCard("");
+  } else {
+    setSelectedCard(id);
+  }
+};
+
+const handleDeleteSection = (e: MouseEvent, index: number) => {
+  e.stopPropagation();
+  setSelectedSections((prevSections: any[]) => {
+    const newSections = Array.from(prevSections);
+    newSections.splice(index, 1);
+    return newSections;
+  });
+};
+
+const handleDuplicateSection = (e: MouseEvent, index: number, data: any) => {
+  e.stopPropagation();
+  setSelectedSections((prevSections: any[]) => {
+    const newSections = Array.from(prevSections);
+    const newSection = { ...data };
+    newSection.id = Math.random().toString(36).substring(2, 9);
+    newSections.splice(index + 1, 0, newSection);
+    return newSections;
+  });
+};
