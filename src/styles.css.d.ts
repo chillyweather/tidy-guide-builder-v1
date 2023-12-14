9ea9e4cb1d5629b615da3da2d8d51dfe7b912682
Adir <slutzkis@gmail.com>
2023-12-14 12:46:13 +0200

    updates

--- src/styles.css.d.ts
+++ src/styles.css.d.ts
@@ -89,6 +89,7 @@ declare const styles: {
   readonly "subtitle": string;
   readonly "text-size": string;
   readonly "textCardBodyContent": string;
+  readonly "textSymbolsCounter": string;
   readonly "tidyLogo": string;
   readonly "title-row": string;
   readonly "title-wrapper": string;
