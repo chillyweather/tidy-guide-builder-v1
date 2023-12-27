66e432a149f3728ca95a9dacd63c17ae80da7d7b
Dmitri Dmitriev <d.dzmitryeu@gmail.com>
2023-12-27 18:04:04 +0200

    Refactor getNode function in documentationBuilder.ts

--- src/figma_functions/documentationBuilder.ts
+++ src/figma_functions/documentationBuilder.ts
@@ -41,31 +41,29 @@ async function getNode(id: string, key: string) {
   const node = figma.getNodeById(id);
   if (
     node &&
-    (node.type === "COMPONENT" || node?.type === "COMPONENT_SET") &&
+    (node.type === "COMPONENT" || node.type === "COMPONENT_SET") &&
     node.key === key
-  )
+  ) {
     return node;
-  else
-    try {
-      try {
-        const importedComponent = await figma.importComponentByKeyAsync(key);
-        return importedComponent;
-      } catch (error) {
-        console.log("error", error);
-      }
-      try {
-        const importedComponentSet =
-          await figma.importComponentSetByKeyAsync(key);
-        return importedComponentSet;
-      } catch (error) {
-        console.log("error", error);
-      }
-    } catch (error) {
-      figma.closePlugin(
-        "Error: No component found for this section. Please import it from the library."
-      );
-      return;
-    }
+  }
+
+  try {
+    const importedComponentSet = await figma.importComponentSetByKeyAsync(key);
+    return importedComponentSet;
+  } catch (error) {
+    console.log("error", error);
+  }
+
+  try {
+    const importedComponent = await figma.importComponentByKeyAsync(key);
+    return importedComponent;
+  } catch (error) {
+    console.log("error", error);
+  }
+
+  figma.closePlugin(
+    "Error: No component found for this section. Please import it from the library."
+  );
 }
 
 export default async function documentationBuilder(data: any) {
