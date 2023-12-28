31b039951e98dc4b59135c08187b75b6da324450
Adir <slutzkis@gmail.com>
2023-12-28 14:23:41 +0200

    Update VideoCard.tsx

--- src/ui_components/sectionCards/VideoCard.tsx
+++ src/ui_components/sectionCards/VideoCard.tsx
@@ -111,7 +111,7 @@ const videoCard = (
             {videoTextBoxElement(
               videoLink,
               setVideoLink,
-              "Enter YouTube video link",
+              "Paste YouTube URL here",
               "videoLink",
               setFoundVideoData,
               false,
