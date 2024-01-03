220dfa83eb9ea8209674f6bae6d227a645e2c45c
Adir <slutzkis@gmail.com>
2024-01-03 12:40:57 +0200

    Update VariantsCard.tsx

--- src/ui_components/sectionCards/VariantsCard.tsx
+++ src/ui_components/sectionCards/VariantsCard.tsx
@@ -4,11 +4,6 @@ import emptyImage from "../../images/empty.svg";
 const VariantsCard = ({}: {}) => {
   return (
     <div className="nothingCards">
-      {/* <img
-        src={emptyImage}
-        alt="Please select a component in Figma."
-        className={"empty-image"}
-      /> */}
       <p>This element will only be visible on canvas</p>
     </div>
   );
