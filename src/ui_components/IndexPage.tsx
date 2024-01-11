0dd24a73ed51d2633798967e22faa3e3c016226d
Dmitri Dmitriev <d.dzmitryeu@gmail.com>
2024-01-11 08:42:08 +0200

    Refactor IndexPage component

--- src/ui_components/IndexPage.tsx
+++ src/ui_components/IndexPage.tsx
@@ -25,9 +25,7 @@ const IndexPage = ({
   const token = useContext(BuilderContext)?.token;
   const [primary, setPrimary] = useState("Reset");
 
-  const{
-    setShowResetPopup,
-  } = useContext(BuilderContext) || {};
+  const { setShowResetPopup } = useContext(BuilderContext) || {};
   if (Object.keys(data).length === 0) return <div>{!!"no data"}</div>;
   const sortedData = data.sort((a: any, b: any) =>
     a.title.localeCompare(b.title)
@@ -57,77 +55,81 @@ const IndexPage = ({
             </div>
             <IconTrash
               className={"trashIcon"}
-              onClick={async (e) => {
-                // console.log('delete-me ' + element._id);
-                document.getElementById("deletePopup")?.classList.remove("hidden");
-                let elementTest = element._id;
-                //@ts-ignore
-                document.getElementById("deletePopup").alt = element._id;
-              }}
-              // onDblClick={async (e) => {
-              //   e.stopPropagation();
-              //   await deleteDocumentation(token!, element._id);
-              //   setDataForUpdate((prevData: any) => {
-              //     const newData = prevData.filter(
-              //       (el: any) => el._id !== element._id
-              //     );
-              //     return newData;
-              //   });
+              // onClick={async (e) => {
+              //   // console.log('delete-me ' + element._id);
+              //   document.getElementById("deletePopup")?.classList.remove("hidden");
+              //   let elementTest = element._id;
+              //   //@ts-ignore
+              //   document.getElementById("deletePopup").alt = element._id;
               // }}
+              onDblClick={async (e) => {
+                e.stopPropagation();
+                await deleteDocumentation(token!, element._id);
+                setDataForUpdate((prevData: any) => {
+                  const newData = prevData.filter(
+                    (el: any) => el._id !== element._id
+                  );
+                  return newData;
+                });
+              }}
             />
           </div>
         );
       })}
-
-<div
-      className={"feedbackPopupBackground hidden"}
-      id={"deletePopup"}
-      onClick={() => document.getElementById("deletePopup")?.classList.add("hidden")}
-      tabIndex={0}
-    >
-      <div className={"feedbackPopup"} onClick={(e) => e.stopPropagation()}>
-        <button
-          className={"closePopupButton"}
-          onClick={() => document.getElementById("deletePopup")?.classList.add("hidden")}
-        >
-          <IconX />
-        </button>
-        <h2 className={"dialogTitle"}>Delete element?</h2>
-        <p>Are you sure you want to delete this element?<br />
-          This action cannot be undone. </p>
-        <div className="popupButtons footer">
-          <button
-            className={"button"}
-            onClick={() => {
-              document.getElementById("deletePopup")?.classList.add("hidden");
-            }}
-            onKeyDown={(e) => {
-              if (e.key === "Escape") document.getElementById("deletePopup")?.classList.add("hidden");
-            }}
-          >
-            Cancel
-          </button>
-          <button
-            className={"button primary"}
-            onClick={async (e) => {
-              e.stopPropagation();
-              await deleteDocumentation(token!, document.getElementById("deletePopup").alt);
-              setDataForUpdate((prevData: any) => {
-                const newData = prevData.filter(
-                  document.getElementById("deletePopup")?.classList.add("hidden");
-                  (el: any) => el._id !== document.getElementById("deletePopup").alt;
-                );
-                return newData;
-              });
-            }}
-          >
-            Delete
-          </button>
-        </div>
-      </div>
-    </div>
     </div>
   );
 };
+
+// {/*
+// <div
+//       className={"feedbackPopupBackground hidden"}
+//       id={"deletePopup"}
+//       onClick={() => document.getElementById("deletePopup")?.classList.add("hidden")}
+//       tabIndex={0}
+//     >
+//       <div className={"feedbackPopup"} onClick={(e) => e.stopPropagation()}>
+//         <button
+//           className={"closePopupButton"}
+//           onClick={() => document.getElementById("deletePopup")?.classList.add("hidden")}
+//         >
+//           <IconX />
+//         </button>
+//         <h2 className={"dialogTitle"}>Delete element?</h2>
+//         <p>Are you sure you want to delete this element?<br />
+//           This action cannot be undone. </p>
+//         <div className="popupButtons footer">
+//           <button
+//             className={"button"}
+//             onClick={() => {
+//               document.getElementById("deletePopup")?.classList.add("hidden");
+//             }}
+//             onKeyDown={(e) => {
+//               if (e.key === "Escape") document.getElementById("deletePopup")?.classList.add("hidden");
+//             }}
+//           >
+//             Cancel
+//           </button>
+//           <button
+//             className={"button primary"}
+//             onClick={async (e) => {
+//               e.stopPropagation();
+//               await deleteDocumentation(token!, document.getElementById("deletePopup").alt);
+//               setDataForUpdate((prevData: any) => {
+//                 const newData = prevData.filter(
+//                   document.getElementById("deletePopup")?.classList.add("hidden");
+//                   (el: any) => el._id !== document.getElementById("deletePopup").alt;
+//                 );
+//                 return newData;
+//               });
+//             }}
+//           >
+//             Delete
+//           </button>
+//         </div>
+//       </div>
+//     </div>
+//     </div>
+//   );
+// }; */}
 
 export default IndexPage;
