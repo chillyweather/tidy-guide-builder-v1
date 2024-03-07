b73cdbcab79c7abd6b4c2fdb01007fcad0aa5f13
Dmitri Dmitriev <d.dzmitryeu@gmail.com>
2024-03-07 12:32:45 +0200

    Refactor MainContent component to use destructuring for props

--- src/ui_components/MainContent.tsx
+++ src/ui_components/MainContent.tsx
@@ -1,14 +1,15 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { h } from "preact";
-import { useEffect } from "preact/hooks";
 import { DraggableCardList } from "./DraggableCardsList";
 import HeaderCard from "./sectionCards/HeaderCard";
 
-const MainContent = (selectedSections: any, setSelectedSections: any) => {
-  useEffect(() => {
-    console.log("selectedSections in MainCard", selectedSections);
-  }, [selectedSections]);
-
+const MainContent = ({
+  selectedSections,
+  setSelectedSections,
+}: {
+  selectedSections: any;
+  setSelectedSections: any;
+}) => {
   return (
     <div className="mainContent">
       <HeaderCard />
