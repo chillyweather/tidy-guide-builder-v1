86d4c84007c10b91c6c094679c4d0869d7813135
Adir <slutzkis@gmail.com>
2024-04-02 14:39:49 +0300

    updates

---
+++ src/ui_components/Dropdown.tsx
@@ -0,0 +1,83 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { h, FunctionalComponent } from "preact";
+import { useState, useEffect } from "preact/hooks";
+import { useAtom } from "jotai";
+import {
+  currentUserIdAtom,
+  currentUserRoleAtom,
+  selectedCollectionAtom,
+} from "src/state/atoms";
+import { findUserRole } from "src/ui_components/ui_functions/findUserRole";
+
+interface DropdownProps {
+  options: any[];
+  onSelect: any;
+}
+
+const CollectionsDropdown: FunctionalComponent<DropdownProps> = ({
+  options,
+  onSelect,
+}) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedOption, setSelectedOption] = useState<string | null>(
+    options[0].name || "🧨 no collection"
+  );
+  const [currentUserId] = useAtom(currentUserIdAtom);
+  const [, setCurentUserRole] = useAtom(currentUserRoleAtom);
+  const [selectedCollection] = useAtom(selectedCollectionAtom);
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const selectOption = (option: any) => {
+    setSelectedOption(option.name);
+    setIsOpen(false);
+    onSelect(option);
+  };
+
+  useEffect(() => {
+    const role = findUserRole(selectedCollection, currentUserId);
+    setCurentUserRole(role);
+  }, [selectedCollection]);
+
+  // useEffect(() => {
+  //   if (options.length > 0 && currentUserId) {
+  //     const role = findUserRole(selectOption, currentUserId);
+  //     console.log("role", role);
+  //     setCurentUserRole(role);
+  //   }
+  // }, [selectOption]);
+
+  return (
+    <div class="dropdown-comp">
+      <button
+        class="dropdown-toggle"
+        onClick={toggleDropdown}
+        onBlur={() => setIsOpen(false)}
+      >
+        {selectedOption || "Select an option"}
+      </button>
+      {isOpen && (
+        <ul className="dropdown-menu">
+          {options.map((option) => {
+            const role = findUserRole(option, currentUserId);
+            return (
+              <li
+                className={"dropdown-item"}
+                onMouseDown={(e) => {
+                  e.preventDefault();
+                  selectOption(option);
+                }}
+              >
+                <div>{option.name}</div> <span className={"tag " + role}></span>
+              </li>
+            );
+          })}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default CollectionsDropdown;
