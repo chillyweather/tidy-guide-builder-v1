64d7acfb51c5a3c1f80c1a7e97049df838ffbf8d
Adir <slutzkis@gmail.com>
2024-04-04 15:33:17 +0300

    Update ui.tsx

--- src/ui.tsx
+++ src/ui.tsx
@@ -622,13 +622,10 @@ function Plugin() {
       className={"container"}
       onClick={() => {
         //@ts-ignore
-        document.getElementById("userMenu").open = false;
         const x = document.getElementsByTagName("details");
         let i;
         for (i = 0; i < x.length; i++) {
-          if (x[i].classList.contains("open") == true) {
             x[i].open = false;
-          }
         }
       }}
       onKeyDown={(e) => {
