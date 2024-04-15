3fc04256ab9d15949025d6bdd3d4fcd97e8126db
Adir <slutzkis@gmail.com>
2024-04-15 14:29:22 +0300

    updates

--- src/ui.tsx
+++ src/ui.tsx
@@ -752,12 +752,17 @@ function Plugin() {
             />
           )}
         {<div className="empty-index">
-        <img src={EmptyState} className={"empty-index-image"} />
+          <img src={EmptyState} className={"empty-index-image"} />
           <div className="empty-index-flex">
             <h2>Looks like you don't have any Documentation</h2>
             <p>Fortunately, it's easy to create documentation</p>
           </div>
-          <button className={"blue-button"}>Start Documenting</button>
+          <button
+            className={"blue-button"}
+            onClick={() => { document.getElementById("new-button")?.click(); }}
+          >
+            Start Documenting
+          </button>
         </div>}
         {showMainContent && !isViewModeOpen && (
           <MainContent
