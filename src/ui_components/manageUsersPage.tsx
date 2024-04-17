b11e1ec82dd7e6d22d3c5e2a1b1620c7f855f1e9
Dmitri Dmitriev <d.dzmitryeu@gmail.com>
2024-04-17 12:11:43 +0300

    Fix unused variables and console logs in manageUsersPage.tsx

--- src/ui_components/manageUsersPage.tsx
+++ src/ui_components/manageUsersPage.tsx
@@ -284,16 +284,13 @@ function AddUserForm({
 }): any {
   const { token } = useContext(BuilderContext) || {};
   if (!token) return null;
-  const [selectedCollection]: any = useAtom(selectedCollectionAtom);
+  // const [selectedCollection]: any = useAtom(selectedCollectionAtom);
   const [userToEdit]: any = useAtom(userToEditAtom);
   const [isAddUserError, setIsAddUserError] = useAtom(isAddUserErrorAtom);
   const [, setAddUserMessage] = useAtom(addUserMessageAtom);
   const [email, setEmail] = useState(userEmail || "");
   const [role, setRole] = useState(userToEdit ? userToEdit.rank : "Viewer");
   const [, setUserToEdit] = useAtom(userToEditAtom);
-
-  console.log("userId", userId);
-  console.log("selectedCollection", selectedCollection);
 
   const handleSubmit = async (e: any) => {
     if (type === "Add") {
@@ -305,7 +302,6 @@ function AddUserForm({
         role
       );
       const message = response.message;
-      console.log("message", message);
       switch (message) {
         case "User already exists in the collection":
           setIsAddUserError(true);
