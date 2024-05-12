c944e62f07afc182cc90576a7059c59b50dc4e5e
Adir <slutzkis@gmail.com>
2024-05-12 15:03:38 +0300

    updates

--- src/styles.css.d.ts
+++ src/styles.css.d.ts
@@ -74,6 +74,7 @@ declare const styles: {
   readonly "dialogTitle": string;
   readonly "disabled": string;
   readonly "divider": string;
+  readonly "dosDontSection": string;
   readonly "dosDonts": string;
   readonly "draft": string;
   readonly "draftComponent": string;
